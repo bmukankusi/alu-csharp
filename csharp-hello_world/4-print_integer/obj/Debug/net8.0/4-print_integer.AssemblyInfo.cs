4dfe9d1d84a26c497d03b10e981ffbc13eccd52e
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-09-07 23:45:28 +0300

    Use .NET version 7

--- csharp-hello_world/4-print_integer/obj/Debug/net8.0/4-print_integer.AssemblyInfo.cs
+++ csharp-hello_world/4-print_integer/obj/Debug/net8.0/4-print_integer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799a55f04ff7590b587f279cbdaebb0e9f1cfcca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9455fe55f779900eda890af72f41ed5e9a8f07a")]
 [assembly: System.Reflection.AssemblyProductAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4-print_integer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp-hello_world/5-print_float/obj/Debug/net8.0/5-print_float.AssemblyInfo.cs
+++ csharp-hello_world/5-print_float/obj/Debug/net8.0/5-print_float.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799a55f04ff7590b587f279cbdaebb0e9f1cfcca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9455fe55f779900eda890af72f41ed5e9a8f07a")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
