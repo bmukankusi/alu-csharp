1d0e57fac431701f3dcad52abc978e8bff925b70
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-07 11:36:49 +0300

    Fix 0-enemy file

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,20 +1,8 @@
-﻿namespace Enemies
-{
+﻿namespace Enemies {
     /// <summary>
     /// Defines a zombie
     /// </summary>
-    class Zombie
-    {
-        // Add properties and methods for the Zombie class here
-    }
+    class Zombie {
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Example usage
-            Zombie zombie = new Zombie();
-            Console.WriteLine("A new zombie has been created.");
-        }
     }
 }
