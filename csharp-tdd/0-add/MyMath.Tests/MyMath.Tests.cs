78cb948f839fbb9098041d7b048d627474c5cfda
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-11 17:49:53 +0300

    .\0-add

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,23 +1,10 @@
-namespace MyMath.Tests;
-
-public class Tests
+namespace MyMath
 {
-    [SetUp]
-    public void Setup()
+    public class Operations
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
-
-   // [Test]
-   // public void Test_sumOfPositive_numbers()
-   // {  
-    //    var result = MyMath.Operations.Add(2,3);
-    //        var expected = 5;
-    //  Assert.That(expected, Is.EqualTo(result));
-    //}
-    [TestCase (2,3,5)]
-    [TestCase (-1,-2,-3)]
-    [TestCase(1,1,2)]
-    public void Test_Addition(int a,int b, int result){
-        Assert.That(result, Is.EqualTo(MyMath.Operations.Add(a,b)));
-    }
-}    
+}
