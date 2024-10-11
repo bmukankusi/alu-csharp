87174acb350e3d45aa600d7890b5eaf5665496d8
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-11 18:03:40 +0300

    Add

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,23 +1,18 @@
-namespace MyMath.Tests;
-
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-   // [Test]
-   // public void Test_sumOfPositive_numbers()
-   // {  
-    //    var result = MyMath.Operations.Add(2,3);
-    //        var expected = 5;
-    //  Assert.That(expected, Is.EqualTo(result));
-    //}
-    [TestCase (2,3,5)]
-    [TestCase (-1,-2,-3)]
-    [TestCase(1,1,2)]
-    public void Test_Addition(int a,int b, int result){
-        Assert.That(result, Is.EqualTo(MyMath.Operations.Add(a,b)));
+        [TestCase(2, 3, 5)]
+        [TestCase(-1, -2, -3)]
+        [TestCase(1, 1, 2)]
+        public void Test_Addition(int a, int b, int result)
+        {
+            Assert.That(result, Is.EqualTo(MyMath.Operations.Add(a, b)));
+        }
     }
-}    
+}
