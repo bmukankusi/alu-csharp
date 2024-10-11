9933646a1bc280a30055c23aa4d63e45b155e0b2
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-11 17:56:50 +0300

    Fix 0-add

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,10 +1,23 @@
-namespace MyMath
+namespace MyMath.Tests;
+
+public class Tests
 {
-    public class Operations
+    [SetUp]
+    public void Setup()
     {
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
     }
-}
+
+   // [Test]
+   // public void Test_sumOfPositive_numbers()
+   // {  
+    //    var result = MyMath.Operations.Add(2,3);
+    //        var expected = 5;
+    //  Assert.That(expected, Is.EqualTo(result));
+    //}
+    [TestCase (2,3,5)]
+    [TestCase (-1,-2,-3)]
+    [TestCase(1,1,2)]
+    public void Test_Addition(int a,int b, int result){
+        Assert.That(result, Is.EqualTo(MyMath.Operations.Add(a,b)));
+    }
+}    
