8fa50fc00d6cb0d7cbeccaf8701d4c383c6183ce
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-11 17:22:25 +0300

    .\0-add

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,15 @@
+﻿namespace MyMath;
+
+    ///<summary>
+    /// class for operations
+    ///</summary>
+public class Operations
+{
+    ///<summary>
+    /// This Method adds two numbers
+    ///</summary>
+    public static int Add(int a, int b) {
+        return a+b;
+    }
+
+}
