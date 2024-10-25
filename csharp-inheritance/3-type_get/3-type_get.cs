23417ae7895f9974f63d55e818d06022a9c7fde5
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-10-25 14:39:52 +0300

    Initialize the properties

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,29 +1,51 @@
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Reflection;
 
-    /// <summary>Represents the class Obj.</summary>
-
-public class Obj
+namespace YourNamespace
 {
     /// <summary>
-    /// prints the names of the available properties and methods of an object.
+    /// Represents the class Obj.
     /// </summary>
-    /// <param name="myObj">Object from where to get informations.</param>
-    public static void Print(object myObj)
+    public class Obj
     {
-        Type type = myObj.GetType();
-        
-        Console.WriteLine($"{type.Name} Properties:");
-        foreach (PropertyInfo prop in type.GetProperties())
+        private string name;
+
+        /// <summary>
+        /// Initializes a new instance of the Obj class.
+        /// </summary>
+        public Obj()
         {
-            Console.WriteLine(prop.Name);
+            name = string.Empty; // Initialize the non-nullable field
         }
-        
-        Console.WriteLine($"{type.Name} Methods:");
-        foreach (MethodInfo method in type.GetMethods())
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name
         {
-            Console.WriteLine(method.Name);
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Prints the names of the available properties and methods of an object.
+        /// </summary>
+        /// <param name="myObj">Object from where to get information.</param>
+        public static void Print(object myObj)
+        {
+            Type type = myObj.GetType();
+
+            Console.WriteLine($"{type.Name} Properties:");
+            foreach (PropertyInfo prop in type.GetProperties())
+            {
+                Console.WriteLine(prop.Name);
+            }
+
+            Console.WriteLine($"{type.Name} Methods:");
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
         }
     }
 }
--- csharp-inheritance/3-type_get/obj/Debug/net7.0/3-type_get.AssemblyInfo.cs
+++ csharp-inheritance/3-type_get/obj/Debug/net7.0/3-type_get.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-type_get")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac66a426119c488f5f982896376f50ba7f086f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f67651009a22b1db450da8a37fb2662134017e")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-type_get")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-type_get")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
