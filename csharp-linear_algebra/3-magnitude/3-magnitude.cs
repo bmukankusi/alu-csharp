f403412751ae2948f105c2bba4c65be106da0ba8
Betty Mukankusi <b.mukankusi@alustudent.com>
2024-11-10 22:54:31 +0300

    Initial commit

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// vector magnitude
+/// </summary>
+public class VectorMath
+{
+    /// <summary>
+/// Calculates the magnitude (Euclidean norm) of a 2D or 3D vector.
+/// </summary>
+/// <param name="vector">The vector for which to calculate the magnitude.</param>
+/// <returns>The magnitude of the vector, or -1 if the vector dimension is not 2 or 3.</returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+        return -1; // Invalid vector dimension
+        }
+
+        double sum = 0;
+        foreach (double element in vector)
+        {
+        sum += Math.Pow(element, 2); // Square each element
+        }
+
+        return Math.Round(Math.Sqrt(sum), 2); // Round to nearest hundredth
+    }
+}
---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+///THIS class contains methods for performing vector operations
+/// </summary>
+public class VectorMath{
+    /// <summary>
+/// Performs element-wise multiplication of a vector by a scalar value.
+/// </summary>
+/// <param name="vector">The vector to be multiplied.</param>
+/// <param name="scalar">The scalar value to multiply by.</param>
+/// <returns>A new vector containing the product of each element in the vector and the scalar.</returns>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length !=2 && vector.Length !=3){
+            return new double[] {-1, -1, -1};
+        }
+        int dimension = vector.Length;
+        double[] result = new double[dimension];
+
+        for (int i = 0; i < dimension; i++)
+        {
+        result[i] = vector[i] * scalar;
+        }
+
+        return result;
+    }
+}
