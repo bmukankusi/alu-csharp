ae9f18f7cceaa319179fc42fc0709a5f5e6a4fc9
Betty Mukankusi <b.mukankusi@alustudent.com>
2025-02-22 11:56:28 +0000

    Commmit

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,26 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/// <summary>
+/// Generic class Queue
+/// </summary>
+/// <typeparam name="T"></typeparam>
 
-///<summary>
-///Generic class Queue
-///</summary>
 public class Queue<T>
 {
     /// <summary>
-    /// method for type checking of generic type
+    /// Head of the Queue
     /// </summary>
-    /// <returns>type of generic type</returns>
+    public Node? head;
+    /// <summary>
+    /// Tail of the Queue
+    /// </summary>
+    public Node? tail;
+    /// <summary>
+    /// Count of the Nodes
+    /// </summary>
+    public int count;
+    /// <summary>
+    /// The Node class
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Value of a node
+        /// </summary>
+        public T? value = default;
+        /// <summary>
+        /// Next pointer of a node
+        /// </summary>
+        public Node? next = null;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="v"></param>
+        public Node(T v)
+        {
+            value = v;
+            next = null;
+        }
+
+    }
+    /// <summary>
+    /// Method that returns the type of the Queue
+    /// </summary>
+    /// <returns></returns>
     public Type CheckType()
     {
         return typeof(T);
     }
-}
 
-class Program
-{
-    static void Main()
+    /// <summary>
+    /// Method that adds an item to the queue
+    /// </summary>
+    /// <param name="value"></param>
+    public void Enqueue(T? value)
     {
-        Queue<int> queue = new Queue<int>();
-        Console.WriteLine(queue.CheckType());
+        Node newNode = new Node(value!);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail!.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+    /// <summary>
+    /// Method to count all the nodes of a queue
+    /// </summary>
+    /// <returns></returns>
+    public int Count()
+    {
+        return count;
     }
 }
